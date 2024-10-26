d0beadf262696e58b818e1fd54a527ed62559719
FizaIrfan3016 <fizafatimairfan@gmail.com>
2024-10-26 08:54:28 -0700

    create todo app with next.js.

---
+++ todo-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import TodoApp from "@/components/TodoApp";
+import type { NextPage } from "next";
+const Home: NextPage = () => {
+return(
+  <div >
+  <TodoApp/>
+  </div>
+)
+}
+
+export default Home
